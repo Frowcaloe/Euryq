49fddd5a94eac6ae7b10afd488804efaba214bf2
Frowcaloe <forgame061601@gmail.com>
2024-09-27 00:23:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,8 @@ let nextClicked = false;
 
 nextBtn.addEventListener("click", () => {
   if (!nextClicked) {
-    question.innerHTML = "Thank you for loving me, with all my shortcomings.";
+    question.innerHTML = "Happy National Crush Day, Eury q🤍 hihi.";
     gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/temaaciii.gif";
-    nextBtn.innerHTML = "reply";
-    nextClicked = true;
-  } else {
-    window.location.href = "https://wa.me/yournumber?text=reply%20message%20whatever";
-  }
+    }
+  
 });
